8f1353b0d3fc646564aaf13107ca572d0b1c0c79
rushi <rushi@DESKTOP-KASF1I7>
2025-06-21 18:29:46 +0530

    chenges by omkar188

---
+++ projectSpringDemo2/src/main/java/com/lombackcontroller/Lombbackcontroller.java
@@ -0,0 +1,31 @@
+package com.lombackcontroller;
+
+import java.sql.DriverManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.SneakyThrows;
+
+@RestController
+public class Lombbackcontroller {
+	
+	private static final Logger log = LoggerFactory.getLogger(Lombbackcontroller.class);
+	
+	@GetMapping("/name/{name}")
+	public void m1(@PathVariable("name") String name)
+	{
+		log.info(name);
+	}
+	@RequestMapping("/sneaky")
+	@SneakyThrows
+	public void m2()
+	{
+		DriverManager.getConnection(null, null, null);
+	}
+
+}
